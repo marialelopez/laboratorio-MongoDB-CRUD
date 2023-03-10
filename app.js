9ae6f3abd3625961496be49a247f7348437f9f0d
Sergio Marentes <sergiomarentes705@gmail.com>
2023-03-10 09:54:28 -0500

    adding initial code to app

---
+++ app.js
@@ -0,0 +1,19 @@
+const createUser = undefined;
+
+const findUsers = undefined;
+
+const findUserById = undefined;
+
+const deleteUserById = undefined;
+
+const updateUser = undefined;
+
+const CRUD = {
+  createUser,
+  findUsers,
+  findUserById,
+  deleteUserById,
+  updateUser,
+};
+
+module.exports = CRUD;
